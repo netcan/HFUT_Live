1c29de8223d8af7ee4332ec9f4b51bd17eff0ef3
netcan <1469709759@qq.com>
2017-05-28 17:04:43 +0800

    聊天信息

--- api.php
+++ api.php
@@ -1,6 +1,8 @@
 <?php
 
 $liveDir = './live/';
+$danmuUrl = 'http://172.18.72.13:2121/';
+// $danmuUrl = 'http://127.0.0.1:2121/';
 session_start();
 
 function getRoom() {
@@ -16,6 +18,26 @@ function getUid() {
 	echo json_encode(session_id());
 }
 
+function send($channel, $content) {
+	global $danmuUrl;
+	$to_uid = '';
+	$post_data = array(
+		'type' => 'publish',
+		'channel' => $channel,
+		'content' => $content,
+		'to' => $to_uid,
+	);
+	$ch = curl_init ();
+	curl_setopt ( $ch, CURLOPT_URL, $danmuUrl );
+	curl_setopt ( $ch, CURLOPT_POST, 1 );
+	curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+	curl_setopt ( $ch, CURLOPT_POSTFIELDS, $post_data );
+	$return = curl_exec ( $ch );
+	curl_close ( $ch );
+	var_export($return);
+}
+
 
 if(isset($_GET['getRoom'])) {
 	getRoom();
@@ -23,4 +45,8 @@ if(isset($_GET['getRoom'])) {
 
 if(isset($_GET['getUid'])) {
 	getUid();
+}
+
+if(isset($_GET['send'])) {
+	send($_GET['channel'], $_GET['send']);
 }
